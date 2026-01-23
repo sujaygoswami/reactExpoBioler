4bc16343c0c42f22627ea3ce084453e60fb997a8
Sujay Goswami <sujaygoswami@Sujays-Mac-mini.local>
2026-01-23 18:14:13 +0530

    update push

--- declarations.d.ts
+++
@@ -1,9 +0,0 @@
-declare module "*.scss" {
-  const content: { [className: string]: any };
-  export default content;
-}
-
-declare module "*.sass" {
-  const content: { [className: string]: any };
-  export default content;
-}
